4afac1e82e2956d454bdf04b94ed2726bc395077
Hicham Said Mansour <82213317+hicham-said-mansour@users.noreply.github.com>
2021-06-03 16:59:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Image from './Cigar.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+    fullName:"Hisham" ,
+    profession:"General Manager" ,
+    bio:"importer of Home Appliance", 
+    image: Image,
+    count: 0,
+   show:false
+  }
 }
 
+incrementCounter= () => {
+  const {count} = this.state;
+  count: this.setState ({count:count + 1})
+}
+
+componentDidMount(){
+  setInterval(this.incrementCounter, 1000);
+}
+
+
+showProfil = ()=>{
+this.setState({show: !this.state.show})
+}
+
+  render() { 
+    return (  
+    <div class='identity'>
+      <button onClick={this.showProfil}>{this.state.show ? "Hide Profile" : "Show Profile"} </button>
+      {this.state.show &&
+      <div>  
+      <h1> {this.state.fullName} </h1>
+      <h3> {this.state.profession} </h3>
+      <p> {this.state.bio} </p>
+      <img src={this.state.image}></img>
+      <h3>The component has been mount since</h3>
+      <h2>{this.state.count}</h2>
+      </div>
+      }
+    </div>
+    );
+  }
+ }
+ 
 export default App;
+
+
